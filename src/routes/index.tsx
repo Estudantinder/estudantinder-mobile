b033c88d7d1370ee25a80e161fe0101a87b9edd1
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-11-20 13:54:27 -0300

    feat(routes): adiciona react-navigation para navegacao entre paginas
    documentacao oficial: https://reactnavigation.org/

---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import React from 'react'
+import Home from '../pages/Home'
+
+const { Navigator, Screen } = createStackNavigator()
+
+export default function Router() {
+  return (
+    <NavigationContainer>
+      <Navigator>
+        <Screen name="Home" component={Home} />
+      </Navigator>
+    </NavigationContainer>
+  )
+}
