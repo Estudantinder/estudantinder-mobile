50b2cc52ff5e20cbdf0f85d08f9b1eb501b3d0eb
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-11-20 12:40:17 -0300

    chore(lint): adiciona configuracao para paths do TS e Babel
    sobre paths do TS: https://blog.rocketseat.com.br/path-mapping-typescript/

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,17 @@
-module.exports = function(api) {
-  api.cache(true);
+/* eslint-disable no-undef */
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {},
+        },
+      ],
+    ],
+  }
+}
