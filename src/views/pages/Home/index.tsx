f2edbd4d1e3118b47cd36f932981abfa8a7c3159
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2021-01-09 12:42:43 -0300

    feat(theme): adiciona novo tema do aplicativo

--- src/views/pages/Home/index.tsx
+++ src/views/pages/Home/index.tsx
@@ -2,16 +2,11 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 import { StatusBar } from 'expo-status-bar'
-import { useTheme } from 'styled-components'
 
 export default function Home() {
-  const theme = useTheme()
-
   return (
     <View style={styles.container}>
-      <Text style={{ color: theme.colors.red }}>
-        Open up App.tsx to start working on your app!
-      </Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   )
--- src/views/styles/theme.ts
+++ src/views/styles/theme.ts
@@ -1,8 +1,17 @@
 const theme = {
-  colors: {
-    red: '#ff0000',
-    green: '#00ff00',
-    blue: '#0000ff',
+  primary: {
+    green: '#0FAD58',
+    purple: '#6D36DA',
+  },
+  background: {
+    light_purple: '#F3EDFF',
+  },
+  input: {
+    background: '#F0F0F0',
+    active_border: '#000000',
+    active_text: '#2D2D2D',
+    placeholder: '#D2D2D2',
+    invalid: '#E50302',
   },
 }
 
