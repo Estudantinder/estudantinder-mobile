baf8cead82b29a451aaf0cfd4c16d602f745d661
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2021-01-12 16:04:37 -0300

    fix(validators): troca tipo de birth_date de Date para string em Person

--- src/main/validators/sign-up/Person.ts
+++ src/main/validators/sign-up/Person.ts
@@ -3,7 +3,7 @@ import * as Yup from 'yup'
 export default function ValidateSignUpPerson() {
   return Yup.object().shape({
     name: Yup.string().required(),
-    birth_date: Yup.date().required(),
+    birth_date: Yup.string().required(),
     gender: Yup.string().optional(),
   })
 }
