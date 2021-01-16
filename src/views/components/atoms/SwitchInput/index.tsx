17fded3e607757958c5002347a26dec0974b7fcb
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2021-01-16 12:05:53 -0300

    feat(components): adiciona componente Switch

---
+++ src/views/components/atoms/SwitchInput/index.tsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { Switch } from 'react-native'
+
+import { useField } from '@unform/core'
+import { useTheme } from 'styled-components'
+
+import { Row } from 'views/styles/globalStyles'
+
+import Styled from './styles'
+
+export interface SwitchInputProps {
+  name: string
+  label: string
+}
+
+interface SwitchRef extends Switch {
+  value: boolean
+}
+
+const SwitchInput: React.FC<SwitchInputProps> = (props) => {
+  const inputRef = useRef<SwitchRef>(null)
+
+  const { fieldName, defaultValue, registerField } = useField(props.name)
+
+  const theme = useTheme()
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    })
+
+    inputRef.current && (inputRef.current.value = defaultValue ? true : false)
+  }, [defaultValue, fieldName, registerField])
+
+  const [isEnabled, setIsEnabled] = useState(defaultValue ? true : false)
+
+  function handleChangeValue() {
+    if (!inputRef.current) return setIsEnabled(!isEnabled)
+
+    inputRef.current.value = !isEnabled
+
+    setIsEnabled(!isEnabled)
+  }
+
+  return (
+    <Row>
+      <Switch
+        ref={inputRef}
+        trackColor={{
+          false: '#767577',
+          true: theme.colors.background.light_purple,
+        }}
+        thumbColor={isEnabled ? theme.colors.primary.purple : '#f4f3f4'}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={handleChangeValue}
+        value={isEnabled}
+      />
+
+      <Styled.InputText>{props.label}</Styled.InputText>
+    </Row>
+  )
+}
+
+export default SwitchInput
