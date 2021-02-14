b31e04b8481542a3de7d3ef0201ca2d5d0cbe941
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2021-02-14 13:41:52 -0300

    feat(student-info): adiciona validator de StudentDetails para edit-target-info

---
+++ packages/student-info/edit-target-info/__tests__/StudentDetailsSchema.test.ts
@@ -0,0 +1,69 @@
+import faker from 'faker'
+
+import StudentMock from 'packages/__mocks__/Student.mock'
+import { StudentDetails } from 'packages/entities/Student'
+import StudentDetailsSchema from 'packages/student-info/validators/StudentDetailsSchema'
+
+describe('student-info/edit-target-info/StudentDetailsSchema', () => {
+  const data: StudentDetails = {
+    bio: StudentMock.bio,
+    subjects: StudentMock.subjects,
+  }
+
+  test('should pass if all values are valid', async () => {
+    expect(await StudentDetailsSchema.isValid(data)).toBe(true)
+  })
+
+  describe('bio:', () => {
+    test('should be required', async () => {
+      expect(
+        await StudentDetailsSchema.isValid({ ...data, bio: undefined })
+      ).toBe(false)
+    })
+
+    test('should be the max length of 256', async () => {
+      expect(
+        await StudentDetailsSchema.isValid({
+          ...data,
+          bio: faker.lorem.paragraphs(6),
+        })
+      )
+    })
+  })
+
+  describe('subjects:', () => {
+    test('should be required', async () => {
+      expect(
+        await StudentDetailsSchema.isValid({ ...data, subjects: undefined })
+      ).toBe(false)
+    })
+
+    test('should be an array with 3 items', async () => {
+      expect(
+        await StudentDetailsSchema.isValid({
+          ...data,
+          subjects: [data.subjects[0]],
+        })
+      ).toBe(false)
+
+      expect(
+        await StudentDetailsSchema.isValid({
+          ...data,
+          subjects: [data.subjects[0], data.subjects[1]],
+        })
+      ).toBe(false)
+
+      expect(
+        await StudentDetailsSchema.isValid({
+          ...data,
+          subjects: [
+            data.subjects[0],
+            data.subjects[1],
+            data.subjects[2],
+            data.subjects[0],
+          ],
+        })
+      ).toBe(false)
+    })
+  })
+})
---
+++ packages/student-info/validators/StudentDetailsSchema.ts
@@ -0,0 +1,15 @@
+import * as Yup from 'yup'
+
+import { StudentDetails } from 'packages/entities/Student'
+
+type DetailsKeys = Record<keyof StudentDetails, unknown>
+
+export default Yup.object().shape<DetailsKeys>({
+  bio: Yup.string()
+    .max(256, 'O máximo de caracteres é 256')
+    .required('Digite sua biografia')
+    .trim(),
+  subjects: Yup.array()
+    .test('length', 'Escolha 3 matérias', (value) => value?.length === 3)
+    .required('Escolha 3 matérias'),
+})
