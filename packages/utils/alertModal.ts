f259c3ee66cae1992aeb08ddebd6a2a5fa244480
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2021-03-08 23:40:01 -0300

    fix: adiciona versão do android que causa erro no aplicativo

--- packages/utils/alertModal.ts
+++ packages/utils/alertModal.ts
@@ -2,7 +2,7 @@ import { Alert } from 'react-native'
 
 export default function alertModal(error: any): void {
   if (error.title) {
-    return Alert.alert(error.title, error.message)
+    return Alert.alert(error.title, error.message || new Error().stack)
   }
 
   return alert(error)
