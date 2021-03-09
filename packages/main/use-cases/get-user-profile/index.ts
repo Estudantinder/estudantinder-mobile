17a0f19aeffc99996b2dc1ff8d60a63d821615a6
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2021-03-09 09:53:20 -0300

    fix(main): adiciona contatos em get-user-profile

--- packages/main/use-cases/get-user-profile/index.ts
+++ packages/main/use-cases/get-user-profile/index.ts
@@ -1,5 +1,6 @@
 import api from 'packages/api'
 import ApiError from 'packages/api/ApiError'
+import User from 'packages/entities/User'
 
 import GetStudentSerializer from '../get-students/GetStudentSerializer'
 
@@ -9,17 +10,20 @@ export default async function GetUserProfileUseCase() {
 
     const student = GetStudentSerializer(response.data)
 
-    return { ...student, email: response.data.email }
+    return new User({
+      ...student,
+      email: response.data.email,
+      contacts: response.data.contacts,
+      password: '',
+    })
   } catch (error) {
     if (error.response) {
-      if (error.response) {
-        throw new ApiError({
-          title: error.response?.data.error || 'SOMETHING WENT WRONG',
-          message:
-            error.response.data.message ||
-            String(JSON.stringify(error.response.data)),
-        })
-      }
+      throw new ApiError({
+        title: error.response?.data.error || 'SOMETHING WENT WRONG',
+        message:
+          error.response.data.message ||
+          String(JSON.stringify(error.response.data)),
+      })
     }
 
     throw error
