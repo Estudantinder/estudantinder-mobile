0debcd9ab2df25121026c99fffc5e0d9adbe861f
Cauã Pinheiro <cauaspinheiro@gmail.com>
2021-08-05 19:04:49 -0300

    feat(main): adiciona botão para mais opções em target-profile

--- packages/main/target-profile/index.tsx
+++ packages/main/target-profile/index.tsx
@@ -1,9 +1,17 @@
 import { Route, useRoute } from '@react-navigation/native'
 import React from 'react'
 
+import { Feather } from '@expo/vector-icons'
+
 import StackPageTemplate from 'packages/components/StackPageTemplate'
 import Student from 'packages/entities/Student'
 import ShowStudent from 'packages/show-student-info'
+import { useToggleThemeContext } from 'packages/styles/context'
+
+import {
+  TargetProfileMoreButton,
+  TargetProfileSettingsContainer,
+} from './styles'
 
 export interface TargetProfileRouteProps {
   student: Modify<Student, { birth_date: number }>
@@ -14,8 +22,20 @@ const TargetProfile: React.FC = () => {
     params: { student },
   } = useRoute<Route<'TargetProfile', TargetProfileRouteProps>>()
 
+  const { theme } = useToggleThemeContext()
+
   return (
     <StackPageTemplate title="Ver Perfil" withoutPadding>
+      <TargetProfileSettingsContainer>
+        <TargetProfileMoreButton
+          onPress={() => {
+            // TODO: Add a button to show more info
+          }}
+        >
+          <Feather name="more-vertical" color={theme.icon.default} size={22} />
+        </TargetProfileMoreButton>
+      </TargetProfileSettingsContainer>
+
       <ShowStudent
         student={{ ...student, birth_date: new Date(student.birth_date) }}
       />
---
+++ packages/main/target-profile/styles.ts
@@ -0,0 +1,18 @@
+import { BorderlessButton } from 'react-native-gesture-handler'
+
+import styled from 'styled-components/native'
+
+export const TargetProfileSettingsContainer = styled.View`
+  position: absolute;
+  right: 0px;
+  top: 0px;
+  padding-right: 24px;
+  justify-content: center;
+  align-items: center;
+`
+
+export const TargetProfileMoreButton = styled(BorderlessButton)`
+  padding: 2px;
+  justify-content: center;
+  align-items: center;
+`
